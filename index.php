c4dfd7abfea0e7683c69f2d9ff512f165af9edb5
Rustam <rusgalimov@yandex.ru>
2020-05-10 19:02:53 +0600

    add project navigation

--- index.php
+++ index.php
@@ -12,21 +12,28 @@ $show_complete_tasks = rand(0, 1);
 // Подключение к БД
 $con = connect_db();
 
-// Получение списока проектов
-$sql_projects = "SELECT * FROM `projects` WHERE `id_user` = 1";
-$result_projects = mysqli_query($con, $sql_projects);
-$project_rows = mysqli_fetch_all($result_projects, MYSQLI_ASSOC);
+// Получение значения текущего id проекта
+$current_project_id = $_GET['prj_id'] ?? 0;
+// Проверка на корректность id проекта. Если не корректный, то возвращаем код 404
+if ($current_project_id) {
+    $validate = validate_project_id($current_project_id, $con);
+    if (!$validate) {
+        http_response_code(404);
+        exit;
+    }
+}
 
-// Получаем список задач
-$sql_tasks = "SELECT `t`.`title`, `t`.`status`, `t`.`date_execute`, `p`.`title` AS `category` FROM `tasks` AS `t` JOIN `projects` AS `p` ON `p`.`id` = `t`.`id_project` WHERE `t`.`id_user` = 1";
-$result_tasks = mysqli_query($con, $sql_tasks);
-$task_rows = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
 
+// Получение списка проектов
+$project_rows = get_project_rows($con);
+
+// Получение списока задач
+$task_rows = get_task_rows($current_project_id, $con);
 
 
 
 // HTML код главной страницы
-$page_content = include_template('main.php', compact('show_complete_tasks', 'project_rows', 'task_rows'));
+$page_content = include_template('main.php', compact('show_complete_tasks', 'current_project_id', 'project_rows', 'task_rows'));
 
 // окончательный HTML код
 $layout_content = include_template('layout.php', compact('page_content', 'title'));
--- templates/main.php
+++ templates/main.php
@@ -4,9 +4,9 @@
                 <nav class="main-navigation">
                     <ul class="main-navigation__list">
                         <?php foreach ($project_rows as $val): ?>
-                        <li class="main-navigation__list-item">
-                            <a class="main-navigation__list-item-link" href="#"><?=$val['title']?></a>
-                            <span class="main-navigation__list-item-count"><?=task_count($task_rows, $val['title'])?></span>
+                        <li class="main-navigation__list-item<?php if (current_project_check($val['id'], $current_project_id)): ?> main-navigation__list-item--active<?php endif; ?>">
+                            <a class="main-navigation__list-item-link" href="index.php?prj_id=<?=$val['id']?>"><?=$val['title']?></a>
+                            <span class="main-navigation__list-item-count"><?=$val['task_count']?></span>
                         </li>
                         <?php endforeach; ?>
                     </ul>
